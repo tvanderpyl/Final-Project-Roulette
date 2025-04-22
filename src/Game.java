1a3610d98a24bfa9e5861d281ad4df655b906f41
trevorvanderpyl <trevor.vanderpyl@menloschool.org>
2025-04-22 11:54:27 -0700

    restart of back end

--- src/Game.java
+++ src/Game.java
@@ -4,13 +4,37 @@ import java.awt.*;
 public class Game{
     private Player player;
     private GameViewer window;
+    private int bets[] = new int[38];
 
     public Game(){
         this.player = new Player();
         this.window = new GameViewer(this);
     }
 
+    public void run(){
+        this.getBet();
+        int choice = this.spin();
+        this.calculate(choice);
+    }
+
+    public void getBet(){
+        // get the bet
+    }
+
+    public int spin(){
+        return (int)(Math.random() * 38);
+    }
+
+    public void calculate(int choice){
+        for (int i = 0; i < 38; i++){
+            if (i == choice) {
+                player.editMoney(bets[i]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Game game = new Game();
+        game.run();
     }
 }
